b9c83012e33c27a8e3f577d91db135e74bd48e8d
cyka <1354250064mdzz@gmail.com>
2019-05-07 20:33:42 +0800

    5/7 20:33

--- pages/remark_page/remark.js
+++ pages/remark_page/remark.js
@@ -8,7 +8,7 @@ Page({
     text: '\n',
     mainComments: [],
     imgUrls: [],
-    indicatorDots: false,
+    indicatorDots: true,
     autoplay: true,
     interval: 3000,
     duration: 1000,
@@ -27,7 +27,33 @@ Page({
     commentIndex: 0,
     pubComment: '',
     thirdSessionKey: '',
-    location: ''
+    location: '',
+    //所有图片的高度  
+    imgheights: [],
+    //图片宽度 
+    imgwidth: 750,
+    //默认  
+    current: 0
+  },
+  imageLoad: function (e) {//获取图片真实宽度  
+    var imgwidth = e.detail.width,
+      imgheight = e.detail.height,
+      //宽高比  
+      ratio = imgwidth / imgheight;
+    console.log(imgwidth, imgheight)
+    //计算的高度值  
+    var viewHeight = 750 / ratio;
+    var imgheight = viewHeight;
+    var imgheights = this.data.imgheights;
+    //把每一张图片的对应的高度记录到数组里  
+    imgheights[e.target.dataset.id] = imgheight;
+    this.setData({
+      imgheights: imgheights
+    })
+  },
+  bindchange: function (e) {
+    // console.log(e.detail.current)
+    this.setData({ current: e.detail.current })
   },
 
   /**
